5ec7af8e56e65027a1cc0d1139a8cf6eb1de9258
NAVIN0507 <navinofficial2005@gmail.com>
2025-08-22 14:05:56 +0530

    feat: implement user webhook handling and caching mechanisms

---
+++ src/features/users/db.ts
@@ -0,0 +1,14 @@
+import { UsersTable } from "@/drizzle/schema";
+import { db } from "@/drizzle/db";
+import { eq } from "drizzle-orm";
+export async function upsertUser (user:typeof UsersTable.$inferInsert){
+  console.log("I am Inside creating user")
+  await db.insert(UsersTable).values(user).onConflictDoUpdate({
+    target:[UsersTable.id],
+    set:user
+  })
+}
+
+export async function deleteUser(id:string){
+  await db.delete(UsersTable).where(eq(UsersTable.id , id))
+}
